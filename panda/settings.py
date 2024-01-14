e3cda82d9f41e4e802099ca87e639e0c5b58e93d
mk62955 <70719431+mk62955@users.noreply.github.com>
2024-01-15 01:21:08 +0530

    Update settings.py

--- panda/settings.py
+++ panda/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -133,7 +133,7 @@ STATICFILES_DIR = (
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','*']
+ALLOWED_HOSTS = ['127.0.0.1','*']
 
 
 # Load environment variables from .env file
